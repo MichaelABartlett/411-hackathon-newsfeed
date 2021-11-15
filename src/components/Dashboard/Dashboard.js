5839bb3f589640968eeba5b6feb465ec71052165
DESKTOP-AONL5AH\owner <michaelbartlett864@gmail.com>
2021-11-14 21:41:42 -0600

    got the search sorking withh only the title

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Dashboard.css'
 
+
 const Dashboard = (props) => {
+
+
     
     return (
         props.stories.map((story, index) => {
             return (
-                <li className="list">
+                <li className="list" key={index}>
                     <h3>{story.title}<a href={story.url} id={"url"}>{` (${story.url})`}</a></h3>
-                    <p>{`${story.points} points | ${story.author} | ${story.time} | ${story.comments} comments`}</p>
+                    <p>{`${story.points} points | ${story.author} | ${story.created_at} | ${story.num_comments} comments`}</p>
                 </li>
             )
         }))
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react/cjs/react.development';
 import './Nav.css'
 
 const Nav = (props) => {
+
+    const [filterTerm, setFilterTerm] = useState()
+    const [lockedTerm, setLockedTerm] = useState()
+
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setFilterTerm(filterTerm)
+        setLockedTerm(filterTerm)
+        setFilterTerm("")
+
+        
+    }
+
+
+
+    useEffect(() => {
+        console.log("locked term in Nav: " + lockedTerm);
+    }, [lockedTerm]);
+
+
     return (
         <div id={"nav-grid"}>
             <h2 id={"title"}>Hacker News Feed</h2>
-            <form>
+            <form onSubmit={handleSubmit} >
             <select id={"nav-select"}>
                     <option value={"blanks"}>Story Name</option>
                     <option value={"blanks2"}>Author</option>
@@ -17,12 +39,12 @@ const Nav = (props) => {
                     <option value={"blanks6"}>Last 12 Hours</option>
                     <option value={"blanks7"}>Last 24 Hours</option>
                 </select>
-                
-                <input id={"searchbar"} type="text" name="name" placeholder={"Enter keyword..."}/>
-                <button id={"searchbutton"} type="submit" name="search" >Search</button>
+                <input id={"searchbar"} type="text" name="name" placeholder={"Enter keyword..."} value={filterTerm} onChange={e => setFilterTerm(e.target.value)} />
+                <button id={"searchbutton"} onclick={() => props.searchSet(lockedTerm)} type="submit" name="search" >Search</button>
             </form>
         </div>
     );
 };
 
 export default Nav;
+
